71f49f79b5f448bb479e66d6e2baadc87c381aac
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-05-16 21:03:53 -0300

    feat: Add a honeypot
    Remove the comments throughout the components

--- src/lib/store.js
+++ src/lib/store.js
@@ -5,21 +5,17 @@ export function calcularTotal() {
     return acumulador + produto.preco * produto.quantidade;
   }, 0);
 
-  // Arredonda para duas casas decimais
   return Math.round(total * 100) / 100;
 }
 
 export function salvarCarrinho() {
-  // Convertemos o array (ou objeto) em string JSON
   localStorage.setItem('cart', JSON.stringify(cart));
 }
 
 export function carregarCarrinho() {
   const cartSalvo = localStorage.getItem('cart');
   if (cartSalvo) {
-    // Se o cart já existir, convertemos de volta para o array/objeto
-    // É importante manter a mesma referência se você estiver trabalhando com reatividade
-    cart.length = 0; // Limpa o array existente sem perder a referência
+    cart.length = 0; 
     cart.push(...JSON.parse(cartSalvo)); 
   }
 }
